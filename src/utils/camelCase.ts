5414bb61d5eef53987cb7512afe4a08b3e28ae29
hrdtbs <harada.tsubasa.0422@gmail.com>
2022-01-10 19:28:30 +0900

    chore: cargo init

--- src/utils/camelCase.ts
+++
@@ -1,6 +0,0 @@
-export const camelCase = (str: string) => {
-  const tmp = str.charAt(0).toLowerCase() + str.slice(1)
-  return tmp.replace(/[-_.](.)/g, (_, group1: string) => {
-    return group1.toUpperCase()
-  })
-}
